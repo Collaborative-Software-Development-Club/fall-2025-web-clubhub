d672df9d3f0889b6708bec2a9db4a3a7cc8c10da
donutdiet <72674098+donutdiet@users.noreply.github.com>
2025-09-17 22:14:37 -0400

    Add placeholder nav in root layout

--- web-app/app/layout.tsx
+++ web-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -27,7 +28,14 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                {children}
+                <header>
+                    <h1>Site Header</h1>
+                    <nav>
+                        <Link href="/">Home</Link> |{" "}
+                        <Link href="/attendance">Attendance</Link>
+                    </nav>
+                </header>
+                <main>{children}</main>{" "}
             </body>
         </html>
     );
