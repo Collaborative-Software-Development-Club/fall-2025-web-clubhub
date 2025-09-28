ce488b65c23b2f22fec96310804096e5aff7e9fa
CrazyMan80052 <crazyman8005@gmail.com>
2025-09-28 15:54:56 -0400

    Use shadcn components

--- web-app/components/popular_clubs/ClubCarousel.tsx
+++ web-app/components/popular_clubs/ClubCarousel.tsx
@@ -1,33 +1,46 @@
 "use client";
 
 import React from "react";
-import { EmblaOptionsType } from "embla-carousel";
-import useEmblaCarousel from "embla-carousel-react";
 import "./embla.css";
+
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselPrevious,
+    CarouselNext,
+} from "../ui/carousel";
 
 type PropType = {
     children: React.ReactNode;
-    options?: EmblaOptionsType;
 };
 
 // Generic carousel component
 const ClubCarousel: React.FC<PropType> = (props) => {
-    const { children, options } = props;
-    const [emblaRef, emblaApi] = useEmblaCarousel(options);
+    const { children } = props;
 
-    // Basically uses the sample code for the embla carousel
     return (
-        <section className="embla">
-            <div className="embla__viewport" ref={emblaRef}>
-                <div className="embla__container">
-                    {React.Children.map(children, (child, index) => (
-                        <div className="embla__slide" key={index}>
-                            {child}
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </section>
+        <Carousel
+            //className="embla w-full max-w-full"
+            opts={{
+                //align: "start",
+                //loop: false,
+            }}
+            //orientation="horizontal"
+        >
+            <CarouselContent className="-ml-2 md:-ml-4">
+                {React.Children.map(children, (child, index) => (
+                    <CarouselItem
+                        key={index}
+                        className="pl-2 md:pl-4 basis-1/1 md:basis-1/2 lg:basis-1/3"
+                    >
+                        {child}
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
     );
 };
 
