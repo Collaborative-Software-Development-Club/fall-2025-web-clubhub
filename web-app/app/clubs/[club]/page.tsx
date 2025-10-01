69487e151f10abf8bee0571de08cb7cfdb8124f1
Junho Hwoang <154561586+JunhoHwoang@users.noreply.github.com>
2025-10-01 12:56:25 -0400

    Created reuseable component for editable text field and simple club overview (#16)
    * Created reuseable component for editable text field and simple club overview
    
    * reused cancelEditing for blur and esc key down

--- web-app/app/clubs/[club]/page.tsx
+++ web-app/app/clubs/[club]/page.tsx
@@ -1,17 +1,37 @@
 import clubs from "@/mock/clubs.json";
+import EditableText from "@/components/clubProfile/EditableText";
+import EditableMultipleText from "@/components/clubProfile/EditableMultipleText";
 
-export default async function ClubLeaderPage({
+export default async function ClubPage({
     params,
 }: {
-    params: { club: string };
+    params: Promise<{ club: string }>;
 }) {
-    const clubId = params.club; // Dynamic route param(used for later fetching)
-    const clubData = clubs[0]; // Mock data for now
+    const { club } = await params;
+    const clubData = clubs[0];
 
     return (
-        <div className="w-full max-w-6xl flex flex-col items-center">
-            Purpose Statement + Membership Info + Meeting times + Meeting
-            Location + etc
+        <div className="w-full max-w-6xl flex flex-col items-center py-5 gap-4">
+            <EditableText
+                id={club}
+                title="Purpose Statement"
+                path="Purpose Statement"
+                initialText={clubData["Purpose Statement"]}
+            />
+            <EditableMultipleText
+                id={club}
+                title="Membership Details"
+                path="Membership Details"
+                initialData={clubData["Membership Details"]}
+            />
+            <EditableText
+                id={club}
+                title="Meeting Time and Location"
+                path="Meeting Time and Location"
+                initialText={
+                    clubData["Meeting Information"]["Meeting Time and Place"]
+                }
+            />
         </div>
     );
 }
