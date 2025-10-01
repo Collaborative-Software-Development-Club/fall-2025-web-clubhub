34058d762591abec7dfd2b83254cf7cf5d07583e
Eli Lambrych <ezlambrych@outlook.com>
2025-10-01 19:43:06 -0400

    Preliminary work on implementing UI for managing leaders, advisors, members, in Members tab of Club profile page

--- web-app/app/clubs/[club]/members/page.tsx
+++ web-app/app/clubs/[club]/members/page.tsx
@@ -1,4 +1,10 @@
 import clubs from "@/mock/clubs.json";
+import { Table, TableBody, TableCaption, TableCell, TableHeader, TableRow } from "@/components/ui/table";
+import Image from "next/image";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Edit, Info, MoreHorizontal, Trash2 } from "lucide-react";
+
 
 export default async function MembersPage({
     params,
@@ -9,9 +15,74 @@ export default async function MembersPage({
     const clubData = clubs[0]; // Mock data for now
 
     return (
-        <div>
-            Board Members(president, vp, treasurer, etc) + Advisors + General
-            Body Members + Total Member Count?
-        </div>
+        <>
+            <h1 className="text-3xl font-bold mb-4">Members of CLUBNAME</h1>
+            <h2 className="text-xl font-semibold">Leaders</h2>
+            <div className="mb-6 rounded-lg border overflow-hidden">
+            <Table>
+                <TableHeader>
+                    <TableRow className="bg-muted font-bold px-2">
+                        <TableCell className="w-14"></TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Role</TableCell>
+                        <TableCell>Email</TableCell>
+                        <TableCell className="text-right w-24">Actions</TableCell>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow className="">
+                        <TableCell className="font-medium">
+                            <Image
+                                src="/placeholder-profile-picture.jpg"
+                                alt="Profile Picture"
+                                className="w-10 h-10 rounded-full mr-2 inline-block"
+                                width={40}
+                                height={40}
+                            />
+                        </TableCell>
+                        <TableCell>
+                            <b>Ted Carter</b>
+                        </TableCell>
+                        <TableCell>President</TableCell>
+                        <TableCell>president@osu.edu</TableCell>
+                        <TableCell className="text-center">
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <Button
+                                        className="cursor-pointer"
+                                        variant="ghost"
+                                        size="icon"
+                                    >
+                                        <MoreHorizontal className="w-4 h-4" />
+                                    </Button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent align="end">
+                                    <DropdownMenuItem
+                                        className="cursor-pointer"
+                                    >
+                                        <Edit className="w-4 h-4 mr-2" />
+                                        Edit
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem
+                                        className="cursor-pointer"
+                                    >
+                                        <Trash2 className="w-4 h-4 mr-2" />
+                                        Delete
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+                
+            </div>
+            <h2 className="text-xl font-semibold">Advisors</h2>
+            <h2 className="text-xl font-semibold">Members</h2>
+            <span className="inline-flex items-center bg-gray-200 text-gray-700 text-xs px-2 py-1 rounded-full mr-2 mb-2 align-middle">
+                <Info className="w-4 h-4 mr-1 text-blue-500" />
+                Only visible to club leaders
+            </span>
+        </>
     );
 }
