777c3af95615b0e63f40fffddcdc0267a1eb8708
Sam Baccei <sam.baccei@gmail.com>
2025-10-01 12:57:35 -0400

    Improvements to webscraper (#10)
    * inital prototype of scraper
    
    * additional fields extracted through scraping of subpage. Additional work may be needed to get email addresses, but not vital for MVP

--- scraper/club-list/main.py
+++ scraper/club-list/main.py
@@ -1,5 +1,4 @@
-from functions import extract_club_info_from_url, save_clubs_to_json
-
+from functions import extract_club_info_from_url, save_clubs_to_json, get_subpage_info
 PAGE_URL = 'https://activities.osu.edu/involvement/student_organizations/find_a_student_org?v=list&c=Columbus'
 FILENAME = "data/club-list.json"
 
@@ -9,3 +8,6 @@ if __name__ == "__main__":
     save_clubs_to_json(clubs, FILENAME)
 
     print(f"Extracted {len(clubs)} clubs → {FILENAME}")
+
+    #get_subpage_info(page_url="https://activities.osu.edu/involvement/student_organizations/find_a_student_org/?i=947e5f63-8c1c-4340-b84e-768cf18c092c&v=card&s=collaborative+software+development&c=Columbus&page=0")
+    #get_subpage_info(page_url="https://activities.osu.edu/involvement/student_organizations/find_a_student_org?i=fd8d4fcc-2f93-4d4a-a9ea-203f2c20fee6&v=list&c=Columbus&page=0")
