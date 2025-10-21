4c9c2beae7f3080868c003337cac95f816c14802
CrazyMan80052 <55330663+CrazyMan80052@users.noreply.github.com>
2025-10-21 15:11:09 -0400

    combine and refactor club cards

--- web-app/app/popular_clubs/popular-clubs-page.tsx
+++ web-app/app/popular_clubs/popular-clubs-page.tsx
@@ -43,6 +43,10 @@ export type Club = {
     attendanceRate?: number; // (decimal: avgAttendance/memberCount) (can be derived ig)
     meetingFrequency?: number; // (idk if we need something like this just example)
     isOpen?: boolean; // - is the club open to joining/can you still apply or open registration
+    description?: string;
+    leader?: string;
+    contactEmail?: string;
+    image?: string;
 };
 
 type PopularClubsPageProps = {
