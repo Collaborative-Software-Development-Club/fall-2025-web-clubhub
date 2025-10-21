85f8e1f82224c5a955a4a36fce3135fdb058590c
EckXon <lanonmetzger01@gmail.com>
2025-10-21 15:54:12 -0400

    QOL + Page Persistence
    Changed logo to link in case users try to access home page in that way,
    implemented local storage to save/load the root page number to display

--- web-app/app/header.tsx
+++ web-app/app/header.tsx
@@ -1,14 +1,20 @@
 "use client"
 
-import { link } from "fs";
 import Link from "next/link";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const linkData = "text-gray-700 hover:text-gray-500 hover:underline active:text-red-700 bold"
 const activeLinkData = "text-gray-700 font-bold hover:text-gray-500 hover:underline active:text-red-700 bold"
 
 export default function Header() {
-    const [page, setPage] = useState(0)
+    const [page, setPage] = useState(() => {
+        const savedRootPage = localStorage.getItem('rootPageNum')
+        return savedRootPage ? JSON.parse(savedRootPage) : 0
+    })
+
+    useEffect(() => {
+        localStorage.setItem('rootPageNum', JSON.stringify(page))
+    }, [page])
 
     function handleClick(pageNum: number) {
         setPage(pageNum);
@@ -21,7 +27,7 @@ export default function Header() {
     return (
     <header className=" bg-gray-200 w-screen p-3 fixed">
         <div className="flex justify-between justify-self-center w-19/20 jus">
-            <h1 className="font-bold text-center">ClubHub</h1>
+            <Link href="/" className="font-bold text-center" onMouseDown={e => {handleClick(0)}}>ClubHub</Link>
             <nav className="flex bg-gray-200 gap-4 w-auto justify-between align-middle">
                 <Link href="/" className={getClassData(0)} onMouseDown={e => {handleClick(0)}}>Home</Link>
                 <Link href="/attendance" className={getClassData(1)} onMouseDown={e => {handleClick(1)}}>Attendance</Link>
