cd7d520b18208406c80d18ad710e8b4225937e1f
Kenny Beaverson <140558157+kbeaverson@users.noreply.github.com>
2025-10-29 17:46:41 -0400

    Kennyb/neon clerk (#38)
    * setup clerk
    
    * Basic neon setup
    
    -Basic clerk + neon setup following this tutorial: https://clerk.com/docs/guides/development/integrations/databases/neon
    -Example table schema and actions included temporarily

---
+++ web-app/app/actions.ts
@@ -0,0 +1,26 @@
+'use server'
+
+import { auth } from '@clerk/nextjs/server'
+//import { UserMessages } from './db/schema'
+import { db } from './db'
+import { eq } from 'drizzle-orm'
+
+/** Example functions and import showing how to interact with the database */
+
+// export async function createUserMessage(formData: FormData) {
+//   const { isAuthenticated, userId } = await auth()
+//   if (!isAuthenticated) throw new Error('User not found')
+
+//   const message = formData.get('message') as string
+//   await db.insert(UserMessages).values({
+//     user_id: userId,
+//     message,
+//   })
+// }
+
+// export async function deleteUserMessage() {
+//   const { isAuthenticated, userId } = await auth()
+//   if (!isAuthenticated) throw new Error('User not found')
+
+//   await db.delete(UserMessages).where(eq(UserMessages.user_id, userId))
+// }
--- web-app/drizzle.config.ts
+++ web-app/drizzle.config.ts
@@ -1,13 +1,17 @@
-import { config } from "dotenv";
-import { defineConfig } from "drizzle-kit";
+import { defineConfig } from 'drizzle-kit'
+import { loadEnvConfig } from '@next/env'
 
-config({ path: ".env" });
+loadEnvConfig(process.cwd())
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL must be a Neon postgres connection string')
+}
 
 export default defineConfig({
-    schema: "./db/schema.ts",
-    out: "./db/migrations",
-    dialect: "postgresql",
-    dbCredentials: {
-        url: process.env.DATABASE_URL!,
-    },
-});
+  dialect: 'postgresql',
+  out: "./db/migrations",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+  schema: './app/db/schema.ts',
+})
