9d82e268df4f998859410f6a088083c0c04b9b9a
Bill092738 <bill092738@gmail.com>
2025-11-05 19:29:06 -0500

    new shcema

--- web-app/app/(admin)/site-admin/temp.sql
+++ web-app/app/(admin)/site-admin/temp.sql
@@ -5,3 +5,18 @@ CREATE TABLE users (
     profile_visibility TEXT NOT NULL DEFAULT 'private',
     bio TEXT
 );
+
+CREATE TABLE clubs (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    visibility TEXT NOT NULL DEFAULT 'public'
+);
+
+CREATE TABLE club_leaders (
+    id TEXT PRIMARY KEY,
+    club_id TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    FOREIGN KEY (club_id) REFERENCES clubs(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
