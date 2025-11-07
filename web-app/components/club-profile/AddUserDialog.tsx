a64ef64a4075cb2b4ff3dbcf76a51c76ae91801c
Eli Lambrych <ezlambrych@outlook.com>
2025-11-06 20:49:49 -0500

    Work on adding functionality to AddUserDialogue, (remove user from picker, changing role name updates state)

--- web-app/components/club-profile/AddUserDialog.tsx
+++ web-app/components/club-profile/AddUserDialog.tsx
@@ -11,11 +11,9 @@ import {
     DialogTrigger,
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import Image from "next/image"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
 import { UserMinus } from "lucide-react"
-import { Ref, RefObject, useRef, useState } from "react"
+import { KeyboardEvent, RefObject, useRef, useState } from "react"
 import { ButtonGroup } from "../ui/button-group"
 
 export enum AddUserType {
@@ -23,17 +21,18 @@ export enum AddUserType {
     Leader
 }
 
-export interface AddUserDialogProps {
-    addUserType: AddUserType
-}
-
-type UserPickerItem = {
+export type UserPickerItem = {
     email: string
     name?: string
     role?: string
 }
 
-export function AddUserDialog({ addUserType }: AddUserDialogProps) {
+export interface AddUserDialogProps {
+    addUserType: AddUserType
+    submitFunc: (pickedUsers: UserPickerItem[]) => void
+}
+
+export function AddUserDialog({ addUserType, submitFunc }: AddUserDialogProps) {
     const [pickedUsers, setPickedUsers] = useState<UserPickerItem[]>([]);
     const emailInputElement:RefObject<HTMLInputElement | null> = useRef(null);
 
@@ -43,9 +42,24 @@ export function AddUserDialog({ addUserType }: AddUserDialogProps) {
         }
         const typedEmail = emailInputElement.current.value;
         if (typedEmail && /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(typedEmail) && !pickedUsers.map((pickedUser) => (pickedUser.email)).includes(typedEmail)) {
-            setPickedUsers([...pickedUsers, { email: typedEmail, name: "John Doe", role: ""}]);
+            setPickedUsers([{ email: typedEmail, name: "John Doe", role: ""}, ...pickedUsers]);
             emailInputElement.current.value = "";
             emailInputElement.current.focus();
+        }
+    }
+
+    function handleRemoveUserFromPicker(index:number) {
+        if (emailInputElement.current === null) {
+            return;
+        }
+        setPickedUsers(prev => prev.filter((_, i) => i !== index));
+        emailInputElement.current!.value = "";
+        emailInputElement.current!.focus();
+    }
+
+    function onEmailInputEnterKey(e: KeyboardEvent<HTMLInputElement>) {
+        if (e.key === 'Enter') {
+            handleAddUserToPicker();
         }
     }
 
@@ -55,7 +69,7 @@ export function AddUserDialog({ addUserType }: AddUserDialogProps) {
                 <DialogTrigger asChild>
                     <Button variant="outline">Open Dialog</Button>
                 </DialogTrigger>
-                <DialogContent className="sm:max-w-[550px]">
+                <DialogContent className="sm:max-w-[550px] h-[600px] flex flex-col">
                     <DialogHeader>
                         <DialogTitle>Add {addUserType === AddUserType.Member ? "Members" : "Leaders"}</DialogTitle>
                         <DialogDescription>
@@ -64,24 +78,37 @@ export function AddUserDialog({ addUserType }: AddUserDialogProps) {
                     </DialogHeader>
                     <div className="flex gap-4 ">
                         <ButtonGroup className="grow">
-                            <Input id="add-user-email" name="email" placeholder="buckeye.1@osu.edu" type="email" ref={emailInputElement} />
+                            <Input id="add-user-email" name="email" placeholder="buckeye.1@osu.edu" type="email" ref={emailInputElement} onKeyDown={onEmailInputEnterKey} />
                             <Button onClick={handleAddUserToPicker}>Add User</Button>
                         </ButtonGroup>
                     </div>
                     <hr/>
-                    {JSON.stringify(pickedUsers)}
-                    <div className="grid gap-4">
+                    <div className="flex flex-col gap-4 grow overflow-y-scroll overflow-x-hidden w-[100%]">
                         {
-                            pickedUsers.map((pickedUsersItem, pickedUsersIdx) => (
-                                <div key={pickedUsersItem.email} className="flex flex-row gap-4 items-center">
+                            pickedUsers.map((item) => ({email: item.email, name: item.name})).map((pickedUsersItem, pickedUsersIdx) => (
+                                <div key={pickedUsersItem.email} className="flex flex-row gap-4 items-center animate-in slide-in-from-top-[4px] fade-in">
                                     <Image src="/placeholder-profile-picture.jpg" width={32} height={32} alt="Placeholder" className="rounded-full bg-black" />
                                     <div className="grid">
                                         <span className="font-semibold text-sm">{pickedUsersItem.name}</span>
                                         <span className="text-muted-foreground text-xs">{pickedUsersItem.email}</span>
                                     </div>
                                     <div className="ml-auto flex flex-row gap-2 items-center">
-                                        <Input type="text" className="max-w-[250px]" placeholder="Role (e.g., Member, VP)" />
-                                        <Button variant="ghost" size="icon">
+                                        <Input 
+                                            type="text" 
+                                            className="max-w-[250px]" 
+                                            placeholder="Role (e.g., Member, VP)" 
+                                            onChange={
+                                                (e) => {
+                                                    setPickedUsers(
+                                                        (old) => {
+                                                            old[pickedUsersIdx].role = e.target.value; 
+                                                            return old;
+                                                        }
+                                                    )
+                                                }
+                                            } 
+                                        />
+                                        <Button variant="ghost" size="icon" onClick={() => handleRemoveUserFromPicker(pickedUsersIdx)}>
                                             <UserMinus />
                                         </Button>
                                     </div>
@@ -94,7 +121,13 @@ export function AddUserDialog({ addUserType }: AddUserDialogProps) {
                             <Button variant="outline">Cancel</Button>
                         </DialogClose>
                         <Button variant="outline">Import CSV</Button>
-                        <Button type="submit">Submit</Button>
+                        <Button type="submit"
+                            onClick={
+                                () => {
+                                    if (submitFunc !== undefined)
+                                    submitFunc(pickedUsers)
+                                }
+                            }>Submit</Button>
                     </DialogFooter>
                 </DialogContent>
             </form>
