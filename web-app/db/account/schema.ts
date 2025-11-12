d11659a2921488376fdb270681c30dcfb192aed7
Caleb Spiker <spiker.41@buckeyemail.osu.edu>
2025-11-12 17:22:34 -0500

    Scraped clubs schema (#50)
    * Database schema for scraped club data
    
    * Updated club profile schema
    
    * Added additional leader roles
    
    * repositioned scraped clubs stuff to discovery
    
    * adjusted schema
    
    * fixed leaderId type on club_leaders
    
    * fix schema errors
    
    ---------
    
    Co-authored-by: ldfMion <lorenzodfmion@gmail.com>

--- web-app/db/account/schema.ts
+++ web-app/db/account/schema.ts
@@ -1,11 +1,11 @@
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTableCreator, text, timestamp } from "drizzle-orm/pg-core";
 
-const withPrefix = (name: string) => "account_" + name;
+const createAccountTable = pgTableCreator((name: string) => "account_" + name);
 
 /**
  * Run npx drizzle-kit push to push the schema to the database
  */
-export const userMessages = pgTable(withPrefix("user_messages"), {
+export const userMessages = createAccountTable("user_messages", {
     user_id: text("user_id").primaryKey().notNull(),
     createTs: timestamp("create_ts").defaultNow().notNull(),
     message: text("message").notNull(),
