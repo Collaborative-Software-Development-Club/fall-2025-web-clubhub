4744fbd41805ab08aec7f328b319f85ba1985c8a
Lorenzo Mion <72419437+ldfMion@users.noreply.github.com>
2025-12-03 06:45:07 -0300

    moved search in main page to search params (#74)

--- web-app/services/discovery/tags-service/tags-service.ts
+++ web-app/services/discovery/tags-service/tags-service.ts
@@ -1,20 +1,37 @@
 import { db } from "@/db";
-import { clubHubTags, tags } from "@/db/schema";
-import { mockTags } from "@/mock/tags";
+import { clubHubTags, clubTags, tags } from "@/db/schema";
 import { TagsService } from "@/services/definition";
+import { desc, eq, sql } from "drizzle-orm";
 
 export const actualTagsService: TagsService = {
     getAllTags,
+    getMostPopularTags,
 };
 
 async function getAllTags() {
     const customTags = await db.select().from(clubHubTags);
-    /*
     const scrapedTags = (await db.select().from(tags)).map((t) => ({
         name: t.name,
         type: "DIRECTORY" as const,
     }));
-    */
-    const scrapedTags = mockTags;
-    return [...customTags, ...scrapedTags];
+    return [...scrapedTags, ...customTags];
+}
+
+async function getMostPopularTags(limit: number) {
+    const rows = await db
+        .select({
+            name: tags.name,
+            usageCount: sql<number>`count(${clubTags.clubId})`.as(
+                "usage_count",
+            ),
+        })
+        .from(tags)
+        .innerJoin(clubTags, eq(clubTags.tag, tags.name))
+        .groupBy(tags.name)
+        .orderBy(desc(sql`usage_count`))
+        .limit(limit);
+    return rows.map((row) => ({
+        ...row,
+        type: "DIRECTORY" as const,
+    }));
 }
