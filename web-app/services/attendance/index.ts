714cfae281c91624b2de584bdf1efd3fe5b5e8fd
ldfMion <lorenzodfmion@gmail.com>
2025-12-03 17:11:01 -0500

    functionality to join and manage clubs in view page

--- web-app/services/attendance/index.ts
+++ web-app/services/attendance/index.ts
@@ -1,5 +1,6 @@
 import { AttendanceService, MeetingsService, MembershipService } from "@/services/definition";
 import { db } from "@/db";
+<<<<<<< HEAD
 import { meetings, roster } from "@/db/attendance/schema";
 import { eq } from "drizzle-orm";
 import { ScrapedClub, scrapedClubsService } from "@/services/discovery/scraped-clubs";
@@ -10,13 +11,27 @@ import { and, between, inArray } from "drizzle-orm";
 import { format } from "date-fns";
 import { attendance } from "@/db/attendance/schema";
 import { desc, count, sql } from "drizzle-orm";
+=======
+import { roster } from "@/db/attendance/schema";
+import { and, eq } from "drizzle-orm";
+import {
+    ScrapedClub,
+    scrapedClubsService,
+} from "@/services/discovery/scraped-clubs";
+import { actualAccountService } from "@/services/account/user-service/account-service";
+import { Account } from "@/services/account/user-service/Account";
+import { auth, currentUser } from "@clerk/nextjs/server";
+>>>>>>> d9e7812 (functionality to join and manage clubs in view page)
 
 export const membershipService: MembershipService = {
     addUserToRoster,
+    joinClub,
     getClubsUserIsAMemberOf,
     getAllMembersFromClub,
+    isMember,
 };
 
+<<<<<<< HEAD
 export const attendanceService: AttendanceService = {
     getPopularClubs,
 };
@@ -26,6 +41,27 @@ export const meetingsService: MeetingsService = {
 };
 
 async function addUserToRoster(email: string, clubId: number, userId?: string): Promise<void> {
+=======
+async function joinClub(clubId: number) {
+    const email = await getUserEmail();
+    addUserToRoster(email, clubId);
+}
+
+async function isMember(clubId: number) {
+    const email = await getUserEmail();
+    const rows = await db
+        .select({})
+        .from(roster)
+        .where(and(eq(roster.email, email), eq(roster.club_id, clubId)));
+    return rows.length > 0;
+}
+
+async function addUserToRoster(
+    email: string,
+    clubId: number,
+    userId?: number,
+): Promise<void> {
+>>>>>>> d9e7812 (functionality to join and manage clubs in view page)
     await db.insert(roster).values({
         email: email,
         user_id: userId,
@@ -37,16 +73,18 @@ async function getClubsUserIsAMemberOf(userId: string): Promise<ScrapedClub[]> {
     const clubIds = await db
         .select({ club_id: roster.club_id })
         .from(roster)
-        .where(eq(roster.user_id, userId))
+        .where(eq(roster.user_id, parseInt(userId)));
 
-    return await Promise.all(clubIds.map((row) => scrapedClubsService.getClub(row.club_id)));
+    return await Promise.all(
+        clubIds.map((row) => scrapedClubsService.getClub(row.club_id)),
+    );
 }
 
 async function getAllMembersFromClub(clubId: string): Promise<Account[]> {
     const userIds = await db
         .select({ user_id: roster.user_id })
         .from(roster)
-        .where(eq(roster.club_id, parseInt(clubId)))
+        .where(eq(roster.club_id, parseInt(clubId)));
 
     if (userIds.length === 0 || userIds[0].user_id === null) return [];
 
@@ -88,4 +126,26 @@ async function getMeetingsForDateRange(clubIds: number[], start: Date, end: Date
         .orderBy(meetings.date);
 
     return meetingRecords;
+}
+    return await Promise.all(
+        userIds.map((row) =>
+            actualAccountService.getUserForId(row.user_id!.toString()),
+        ),
+    );
+}
+
+async function getUserEmail() {
+    const { isAuthenticated } = await auth();
+    if (!isAuthenticated) {
+        throw new Error("User not signed in.");
+    }
+    const user = await currentUser();
+    if (!user) {
+        throw new Error("Unable to retrieve current user information.");
+    }
+    const email = user.primaryEmailAddress;
+    if (!email) {
+        throw new Error("User does not have an email address.");
+    }
+    return email.emailAddress;
 }
